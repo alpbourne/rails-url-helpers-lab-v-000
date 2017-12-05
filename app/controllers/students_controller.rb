c57543b33a320f08a9cff0a3845b88c48d9739ac
alpbourne <alpbourne@gmail.com>
2017-12-05 01:22:11 +0000

    automatically backed up by learn

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -1,11 +1,12 @@
 class StudentsController < ApplicationController
   before_action :set_student, only: :show
-  
+
   def index
     @students = Student.all
   end
 
   def show
+    @student = Student.find(params[:id])
   end
 
   def activate_student
@@ -14,7 +15,7 @@ class StudentsController < ApplicationController
     @student.save
     redirect_to student_path(@student)
   end
-  
+
   private
 
     def set_student
